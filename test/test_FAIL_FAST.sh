b554f64d5672f89405dfeeee586d819ffab92e50
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-03 17:23:01 +0100

    Clean logged output of test_FAIL_FAST.sh

--- test/test_FAIL_FAST.sh
+++ test/test_FAIL_FAST.sh
@@ -13,21 +13,19 @@ test_fail() {
   true
 }
 
-[ $# -eq 0 ] || { run_tests "$@"; exit 0; }
-
 set_test_name "Any command that fails in the body of a test function should make the test to fail"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! bash -c "$0 test_fail"
+! CURRENT_TEST_NAME= bash -c "run_tests test_fail"
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
 
 set_test_name "FAIL_FAST should interrupt the script at the first test failure"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! bash -c "$0 test_fail test_ok"
+! CURRENT_TEST_NAME= bash -c "run_tests test_fail test_ok 3>/dev/null"
 ! [ -f "$TEST_SCRIPT_DIR"/.test_ok ]
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
 
 set_test_name "not FAIL_FAST should run all tests but signal failure at the end"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! FAIL_FAST=0 bash -c "$0 test_fail test_ok"
+! FAIL_FAST=0 CURRENT_TEST_NAME= bash -c "run_tests test_fail test_ok"
 [ -f "$TEST_SCRIPT_DIR"/.test_ok ]
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
