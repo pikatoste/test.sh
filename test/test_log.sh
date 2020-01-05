4c380fa9f900416c198e3dfa573f28c799313f09
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-05 06:06:16 +0100

    Clean logged output of test_log.sh

--- test/test_log.sh
+++ test/test_log.sh
@@ -4,7 +4,7 @@ source "$TEST_SCRIPT_DIR"/../test.sh
 
 set_test_name "The log file should contain test stdout and stderr"
 # run in a different test script to avoid the race condition on the log output
-"$TEST_SCRIPT_DIR"/do_test_log.sh
+CURRENT_TEST_NAME= "$TEST_SCRIPT_DIR"/do_test_log.sh
 
 grep ^output$ "$(dirname "$TESTOUT_FILE")"/do_test_log.sh.out
 grep ^stderr$ "$(dirname "$TESTOUT_FILE")"/do_test_log.sh.out
