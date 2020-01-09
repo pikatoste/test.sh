b82a58eb9feeb6c6b480f0c6953ef2c7d9a60d02
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-10 00:20:12 +0100

    Improved message of test_inline.sh

--- test/test_inline.sh
+++ test/test_inline.sh
@@ -2,7 +2,7 @@ TEST_SCRIPT="$(readlink -f "$0")"
 TEST_SCRIPT_DIR=$(dirname "$TEST_SCRIPT")
 source "$TEST_SCRIPT_DIR"/../test.sh
 
-set_test_name "Inline test fails should reflect the failed test in the output"
+set_test_name "Inline test failures should display the failed test in the main output"
 # run in a different test script let this test pass
 ! CURRENT_TEST_NAME= "$TEST_SCRIPT_DIR"/do_test_inline.sh
 
