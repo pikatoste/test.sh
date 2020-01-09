6883bb8df6aa7f021ebcea07705b56a6c740d6af
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-09 15:59:31 +0100

    Fix layout reference in generated front matter

--- tools/jekyllize.sh
+++ tools/jekyllize.sh
@@ -1,5 +1,5 @@
 # Adapt html stdin to jekyll theme
 ORIG="$1"
-awk 'BEGIN {print "---"; print "default"; print "---";} {print}' <"$ORIG" >"${1}".tmp
+awk 'BEGIN {print "---"; print "layout: default"; print "---";} {print}' <"$ORIG" >"${1}".tmp
 rm "$ORIG"
 mv "${1}".tmp "$ORIG"
