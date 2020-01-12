a3d36571fefbff69941b046c50ed1c5c43278009
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-12 05:35:14 +0100

    STACK_TRACE modes: no, pruned, compact, full

--- test/test_REENTER_0.sh
+++ test/test_REENTER_0.sh
@@ -1,3 +1,5 @@
+FAIL_FAST=
+SUBSHELL=always
 source "$(dirname "$(readlink -f "$BASH_SOURCE")")"/../test.sh
 
 test_01() {
@@ -6,3 +8,4 @@ test_01() {
 
 set_test_name "Test config REENTER=0"
 CURRENT_TEST_NAME= REENTER=0 run_tests
+# TODO: check ... something
