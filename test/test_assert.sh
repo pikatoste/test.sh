05b948e620499d24dfe8cbc1304a06fe033fdf85
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-14 13:04:39 +0100

    Improved test_assert.sh

--- test/test_assert.sh
+++ test/test_assert.sh
@@ -1,24 +1,16 @@
-test_01_ok() {
-  start_test "Assertions should not fail when the assertion succeeds"
-  assert_true "true" "ok"
-  assert_false "false" "nok"
-}
-
-test_02_fail() {
-  start_test "assert_true should fail when the assertion is false"
-  assert_true "true" "ok"
-  assert_true "false" "nok"
-}
-
-test_03_fail() {
-  start_test "assert_false shoud fail when the assertion is true"
-  assert_false "true" "ok"
-  assert_false "false" "nok"
-}
-
+# TODO: check with different values of SUBSHELL
 source "$(dirname "$(readlink -f "$BASH_SOURCE")")"/../test.sh
 
-! subshell "FAIL_FAST= SUBSHELL=always run_tests" || false
+start_test "Assertions should not fail when the assertion succeeds"
+assert_true "true" "ok"
+assert_false "false" "nok"
 
+start_test "assert_true should fail when the assertion is false"
+! subshell "assert_true \"false\"" || false
+
+start_test "assert_false shoud fail when the assertion is true"
+! subshell "assert_false \"true\"" || false
+
+# TODO: check for FAIL_FAST, does not belong here
 start_test "Failed assertions should interrupt the test when FAIL_FAST is true"
 ! CURRENT_TEST_NAME= "$TEST_SCRIPT_DIR"/do_test_assert_nosubshell.sh || false
