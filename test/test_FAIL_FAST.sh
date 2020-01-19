ddf37296d03c20f56f7dc42021596366ae8dd2d4
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-19 19:53:36 +0100

    Avoid leaking tested tests output to the main output (Closes #46)

--- test/test_FAIL_FAST.sh
+++ test/test_FAIL_FAST.sh
@@ -16,17 +16,17 @@ source "$(dirname "$(readlink -f "$0")")"/../test.sh
 
 start_test "Any command that fails in the body of a test function should make the test to fail"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! CURRENT_TEST_NAME= subshell "run_tests test_fail" || false
+! CURRENT_TEST_NAME= subshell "run_tests test_fail" 3>&1 || false
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
 
 start_test "When FAIL_FAST is true the first test failure should interrupt the script"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! CURRENT_TEST_NAME= subshell "run_tests test_fail test_ok 3>/dev/null" || false
+! CURRENT_TEST_NAME= subshell "run_tests test_fail test_ok" 3>&1 || false
 ! [ -f "$TEST_SCRIPT_DIR"/.test_ok ] || false
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
 
 start_test "When FAIL_FAST is false failures should not interrupt the script but signal failure at the end"
 rm -f "$TEST_SCRIPT_DIR"/.test_ok "$TEST_SCRIPT_DIR"/.test_fail
-! FAIL_FAST= SUBSHELL=always CURRENT_TEST_NAME= subshell "run_tests test_fail test_ok" || false
+! FAIL_FAST= SUBSHELL=always CURRENT_TEST_NAME= subshell "run_tests test_fail test_ok" 3>&1 || false
 [ -f "$TEST_SCRIPT_DIR"/.test_ok ]
 [ -f "$TEST_SCRIPT_DIR"/.test_fail ]
