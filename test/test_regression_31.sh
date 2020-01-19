b0983dc989c2604ba2f240b0c0c59c3f1320585b
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-19 07:45:38 +0100

    Closes #31

---
+++ test/test_regression_31.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SUBSHELL=never
+source "$(dirname "$(readlink -f "$0")")"/../test.sh
+
+start_test "Regression #31: double error reporting"
+assert_equals "$(false)" ""
+! subshell "run_test_script ./__I_dont_exist" && print_stack_trace || false
