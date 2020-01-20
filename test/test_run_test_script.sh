aacc441b8ccb56df903e5ad9a0b666e838378664
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-20 17:00:19 +0100

    Closes #63: run_test_script: relative paths from the calling script
    Other changes not related to this issue:
    
    * Document COLOR configuration variable.
    * Document the configuration flow and how it affects errors generated during
    configuration parsing.

---
+++ test/test_run_test_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source "$(dirname "$(readlink -f "$0")")"/../test.sh
+
+COMPANION_TEST_NAME=do_$(basename "$TEST_SCRIPT")
+COMPANION_TEST_SCRIPT=$TEST_SCRIPT_DIR/$COMPANION_TEST_NAME
+
+start_test "run_test_script() interprets relative paths from the current script (#63)"
+( cd ..; run_test_script $COMPANION_TEST_NAME )
+diff - "$LOG_DIR"/"$COMPANION_TEST_NAME".out <<EOF
+lorem ipsum
+EOF
