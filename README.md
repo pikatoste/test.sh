0d6db2df0b13bd6e3a2dcd1c4cce546b879d9be1
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-20 11:19:34 +0100

    Improve bash compatibility test and information
    Partially implements #51.

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ See https://pikatoste.github.io/test.sh/.
 
 test.sh is a bash library for writing tests as shell scripts.
 
-Only GNU bash is supported. Requires bash \>=4.4.20. test.sh has been developed with bash version 4.4.20.
-It has been tested succesfully with versions up to 5.0.11.
+Only GNU bash is supported. Requires bash \>=4.4.12. It has been tested succesfully with versions up to 5.0.11.
+test.sh is developed with bash version 4.4.20.
 
 ## Installation
 
--- tools/test-bash-compat.sh
+++ tools/test-bash-compat.sh
@@ -1,7 +1,12 @@
-for bash_version in 4.4 5.0 devel; do
+TEST_SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+LOG_DIR="$TEST_SCRIPT_DIR"/testout
+for bash_version in 4.4.12 4.4.18 4.4 5.0 devel; do
   bash_version_str=$(docker run bash:$bash_version bash --version | head -1)
   echo "Testing bash:$bash_version ($bash_version_str)"
-  docker run -v ~/bbva/test.sh/main:/mnt/test.sh  bash:$bash_version bash -c 'ln -s /usr/local/bin/bash /bin/bash; cd /mnt/test.sh; find runtest -type f -name test_\*.sh | while read test; do "$test"; done' >/dev/null
+  mkdir -p "$LOG_DIR"
+  LOG_FILE="$LOG_DIR"/bash-$bash_version.log
+  echo "Testing bash:$bash_version ($bash_version_str)" >"$LOG_FILE"
+  docker run -v ~/bbva/test.sh/main:/mnt/test.sh  bash:$bash_version bash -c 'set -e; ln -s /usr/local/bin/bash /bin/bash; cd /mnt/test.sh; find runtest -type f -name test_\*.sh | while read test; do "$test"; done' >/dev/null >>"$LOG_FILE"
   if [[ $? = 0 ]]; then
     echo "SUPPORTED: bash:$bash_version ($bash_version_str)"
   else
