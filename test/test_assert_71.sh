b340a667611d5b34889ca5c627a008611f1b12ce
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-22 01:20:35 +0100

    Fix test_assert_71.sh
    Make it independent on where it is run from. Broke coverage.

--- test/test_assert_71.sh
+++ test/test_assert_71.sh
@@ -6,7 +6,7 @@ COMPANION_TEST_NAME=do_$(basename "$TEST_SCRIPT")
 
 for SUBTEST_SUBSHELL in never always; do
   start_test "#71: Assertion failures should not disable further error reporting when SUBSHELL=$SUBTEST_SUBSHELL"
-  ( ! SUBSHELL=$SUBTEST_SUBSHELL COLOR=no run_test_script "$COMPANION_TEST_NAME" || false )
+  ( ! SUBSHELL=$SUBTEST_SUBSHELL PRUNE_PATH="*/" COLOR=no run_test_script "$COMPANION_TEST_NAME" || false )
   EXPECTED_OUT=$FILES_DIR/do_test_assert_71-$SUBTEST_SUBSHELL.out
   CURRENT_OUT=$TEST_SCRIPT_DIR/testout/do_test_assert_71.sh.out
   sed -e 's/:[0-9]*)/:)/' "$CURRENT_OUT" | diff "$EXPECTED_OUT" -
