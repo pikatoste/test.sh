e757ecffff7f8900c7c7088461e900d54d4e9079
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-23 22:58:24 +0100

    Closes #77: test_test_assert_71.sh ok in bash 4&5
    Different sets of expected results for bash 4 and 5.

--- test/test_assert_71.sh
+++ test/test_assert_71.sh
@@ -4,10 +4,11 @@ source "$(dirname "$(readlink -f "$0")")"/../test.sh
 FILES_DIR=$TEST_SCRIPT_DIR/files
 COMPANION_TEST_NAME=do_$(basename "$TEST_SCRIPT")
 
+# TODO: implementar captura de resultados esperados, demasiado tedioso de acutalizar a mano
 for SUBTEST_SUBSHELL in never always; do
   start_test "#71: Assertion failures should not disable further error reporting when SUBSHELL=$SUBTEST_SUBSHELL"
   ( ! SUBSHELL=$SUBTEST_SUBSHELL PRUNE_PATH="*/" COLOR=no run_test_script "$COMPANION_TEST_NAME" || false )
-  EXPECTED_OUT=$FILES_DIR/do_test_assert_71-$SUBTEST_SUBSHELL.out
+  EXPECTED_OUT=$FILES_DIR/do_test_assert_71-$SUBTEST_SUBSHELL-bash${BASH_VERSINFO[0]}.out
   CURRENT_OUT=$TEST_SCRIPT_DIR/testout/do_test_assert_71.sh.out
   sed -e 's/:[0-9]*)/:)/' "$CURRENT_OUT" | diff "$EXPECTED_OUT" -
 done
