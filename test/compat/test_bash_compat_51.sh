bb5abf54a4a274548b68b939f681e754ef5481ae
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-25 02:07:22 +0100

    Test for bash compatibility (#51)

---
+++ test/compat/test_bash_compat_51.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+build_docker_image() {
+  sed -e s/@IMAGE_VERSION@/$1/ "$TEST_SCRIPT_DIR"/Dockerfile.template >"$TEST_SCRIPT_DIR"/Dockerfile
+  docker build -t test.sh:$bash_version --rm "$TEST_SCRIPT_DIR"
+}
+
+setup_test() {
+  # build docker image if not already built
+  docker image ls test.sh:$bash_version 2>/dev/null | grep -q $bash_version || build_docker_image $bash_version
+}
+
+teardown_test() {
+  docker container ls -a | grep test.sh | cut -d \  -f 1 | xargs docker container rm 2>/dev/null || true
+}
+
+source "$(dirname "$(readlink -f "$0")")"/../../test.sh
+
+for bash_version in 4.4.23 5.0.11; do
+  start_test "Bash version $bash_version should be supported"
+  run_test_script do_test_bash_compat_51.sh
+  cp "$LOG_DIR"/do_test_bash_compat_51.sh.out "$TEST_SCRIPT_DIR"/"$LOG_DIR_NAME"-$bash_version/main.out
+done
--- tools/test-bash-compat.sh
+++ tools/test-bash-compat.sh
@@ -10,26 +10,31 @@ build_docker_image() {
   local DOCKER_DIR=$TEST_SCRIPT_DIR/bash
   cp "$DOCKER_DIR"/Dockerfile.template "$DOCKER_DIR"/Dockerfile
   sed -i -e s/@VERSION@/$VERSION/ -e s/@PATCH_LEVEL@/$PATCH_LEVEL/ -e s/@LATEST_PATCH@/$LATEST_PATCH/ "$DOCKER_DIR"/Dockerfile
-  docker build -t bash:$bash_version --rm "$DOCKER_DIR"
+  docker build -t bash-test:$bash_version --rm "$DOCKER_DIR"
 }
 
 default_TEST_BASH_VERSIONS="4.4.{0,23} 5.0.{0,11}"
 TEST_BASH_VERSIONS="$*"
 TEST_BASH_VERSIONS=${TEST_BASH_VERSIONS:-$(eval echo $default_TEST_BASH_VERSIONS)}
+FAILED=0
 for bash_version in $TEST_BASH_VERSIONS; do
-  docker image ls bash:$bash_version 2>/dev/null | grep -q $bash_version || build_docker_image $bash_version
-  bash_version_str=$(docker run bash:$bash_version bash --version | head -1)
+  docker image ls bash-test:$bash_version 2>/dev/null | grep -q $bash_version || build_docker_image $bash_version
+  bash_version_str=$(docker run bash-test:$bash_version bash --version | head -1)
   echo "Testing bash:$bash_version ($bash_version_str)"
   mkdir -p "$LOG_DIR"
   LOG_FILE="$LOG_DIR"/bash-$bash_version.log
   echo "Testing bash:$bash_version ($bash_version_str)" >"$LOG_FILE"
-  docker run -v ~/bbva/test.sh/main:/mnt/test.sh --user test --rm bash:$bash_version bash -c 'set -e; cd ; cp -a /mnt/test.sh/runtest . ;  find runtest -type f -name test_\*.sh | sort | while read test; do "$test"; done' >/dev/null >>"$LOG_FILE"
+  docker run -v "$TEST_SCRIPT_DIR"/..:/mnt/test.sh --user test --rm bash-test:$bash_version bash -c \
+    'set -e; cd ; cp -a /mnt/test.sh/runtest . ;  find runtest -type f -name test_\*.sh | grep -v /compat/ | sort | while read test; do "$test"; done' #2>/dev/null >>"$LOG_FILE"
   if [[ $? = 0 ]]; then
     echo "SUPPORTED: bash:$bash_version ($bash_version_str)"
   else
     echo "NOT SUPPORTED: bash:$bash_version ($bash_version_str)"
+    ((FAILED++))
   fi
 done
 
 echo Remove dangling containers:
-docker container ls -a | grep bash | cut -d \  -f 1 | xargs docker container rm
+docker container ls -a | grep bash-test | cut -d \  -f 1 | xargs docker container rm
+
+exit $FAILED
