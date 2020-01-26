5c6ae996e4e808b941190db7bb5095df4ae49982
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-27 00:00:42 +0100

    Closes #93: cache pruned paths

---
+++ test/test_prune_path_cache.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source "$(dirname "$(readlink -f "$0")")"/../test.sh
+
+start_test "#93: prune_path should put in the cache missing paths"
+PRUNE_PATH_CACHE=()
+PRUNEFILE="$TEST_SCRIPT_DIR"/pepe
+touch "$PRUNEFILE"
+PRUNE_PATH= prune_path "$PRUNEFILE"
+assert_equals "$PRUNEFILE" "${PRUNE_PATH_CACHE[$PRUNEFILE]}"
+
+start_test "#93: prune_path should retrieve cached paths from the cache"
+realpath() {
+  unset realpath
+  fail "realpath called, should have hit the cache"
+}
+PRUNE_PATH="*/" prune_path "$PRUNEFILE"
+unset realpath
+assert_equals "$PRUNEFILE" "${PRUNE_PATH_CACHE[$PRUNEFILE]}"
+
+start_test "#93: init_prune_path_cache reprocesses the preinitialized cache in subtests"
+PRUNE_PATH_CACHE=([/hola/pepe]=/hola/pepe)
+SUBTEST= PRUNE_PATH="*/" INITIALIZE_SOURCE_CACHE= init_prune_path_cache
+assert_equals pepe "${PRUNE_PATH_CACHE[/hola/pepe]}"
