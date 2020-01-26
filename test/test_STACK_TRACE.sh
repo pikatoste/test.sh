87688355b06c3c4cbd22df3cae96fabe5c4b7fc2
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-01-26 03:43:20 +0100

    Use array variable for the stack trace (#87)
    The stack file remains as a means to communicate state across subshells.

--- test/test_STACK_TRACE.sh
+++ test/test_STACK_TRACE.sh
@@ -8,7 +8,8 @@ start_test "STACK_TRACE should accept only valid values"
   for i in no full; do
     STACK_TRACE=$i load_config
   done
-  ! STACK_TRACE=pepe subshell load_config || false
+  STACK_TRACE=pepe result_of load_config
+  [[ $LAST_RESULT != 0 ]]
 )
 
 start_test "When STACK_TRACE=no no stack traces should be produced"
