ed5a44f8de93907d5e10c7306de17146ba54ef38
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-02-13 00:38:42 +0100

    Log time in each bash compatibiity test

--- test/compat/do_test_bash_compat_51.sh
+++ test/compat/do_test_bash_compat_51.sh
@@ -3,4 +3,4 @@
 source "$(dirname "$(readlink -f "$0")")"/../../test.sh
 
 docker run -v "$TEST_SCRIPT_DIR"/../..:/mnt/runtest -u $(id -u) --rm test.sh:$bash_version bash -c \
-  "set -e; cd /mnt/runtest; rm -rf test/testout; make -C test BUSYBOX=1; mv test/testout test/compat/testout-$bash_version"
+  "set -e; cd /mnt/runtest; rm -rf test/testout; time make -C test BUSYBOX=1; mv test/testout test/compat/testout-$bash_version"
