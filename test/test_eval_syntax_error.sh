73eee6b5af0b32f9eda574535c4b421fc8659d75
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-02-26 01:27:05 +0100

    Include the stderr from eval in eval_syntax exception message

--- test/test_eval_syntax_error.sh
+++ test/test_eval_syntax_error.sh
@@ -4,8 +4,8 @@ source "$(dirname "$(readlink -f "$0")")"/../test.sh
 
 start_test "#98: eval syntax errors in the expression of assert_success are reported as such and not as an assertion failure"
 generate_test_fail_check 'assert_success "[[ = b ]]"' <<EOF
-$TESTSH: eval: line : conditional binary operator expected
 [test.sh] eval_syntax exception: Syntax error in the expression: [[ = b ]]
+[test.sh] $TESTSH: eval: line : conditional binary operator expected
 [test.sh]  at _eval(test.sh:)
 [test.sh]  at assert_success(test.sh:)
 [test.sh]  at main(the_test.sh:)
@@ -13,8 +13,8 @@ EOF
 
 start_test "#98: eval syntax errors in the expression of assert_failure are reported as such and not as an assertion failure"
 generate_test_fail_check 'assert_failure "[[ = b ]]"' <<EOF
-$TESTSH: eval: line : conditional binary operator expected
 [test.sh] eval_syntax exception: Syntax error in the expression: [[ = b ]]
+[test.sh] $TESTSH: eval: line : conditional binary operator expected
 [test.sh]  at _eval(test.sh:)
 [test.sh]  at assert_failure(test.sh:)
 [test.sh]  at main(the_test.sh:)
@@ -24,6 +24,6 @@ start_test "#98: eval syntax errors in try/catch nonzero are not caught"
 STACK_TRACE=no generate_test_fail_check 'try: _eval "[[ = b ]]"
 catch nonzero: true
 endtry' <<EOF
-$TESTSH: eval: line : conditional binary operator expected
 [test.sh] eval_syntax exception: Syntax error in the expression: [[ = b ]]
+[test.sh] $TESTSH: eval: line : conditional binary operator expected
 EOF
