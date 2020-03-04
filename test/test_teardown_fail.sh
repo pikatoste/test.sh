46e4a8a922bf74c34db33bea10e9a4acac5507d8
Arturo Garcia Ares <arturo.garcia.ares.next@bbva.com>
2020-03-04 02:57:31 +0100

    Updated tests

--- test/test_teardown_fail.sh
+++ test/test_teardown_fail.sh
@@ -12,8 +12,8 @@ start_test "Teardown functions should print a warning in the main output when th
 OUT="$TEST_SCRIPT_DIR"/.do_test_teardown_fail.sh.mainout
 ( COLOR=no run_test_script do_test_teardown_fail.sh >"$OUT" )
 diff - "$OUT" <<EOF
-* test_01
 WARN: teardown_test failed
+* test_01
 WARN: teardown_test_suite failed
 EOF
 rm "$OUT"
